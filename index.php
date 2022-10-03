78e4560c40a35c975441dba30b8f3cce01dc48e3
black-lite <yamanowitaly@gmail.com>
2022-10-03 22:40:26 +0700

    start generate link and script!!! :)

--- index.php
+++ index.php
@@ -16,6 +16,11 @@ $tpl = new Template;
 Controller::controllerDo();
 $tpl->render();
 
-$act = new \Core\Action('main', 'null', 'id=__id', 's=__s&l=__l&id=__id');
-$act->link('test', ['id' => 5]);
+$date = new \Core\Action('main', 'null', 'id=__id&date=__date', 's=__s&l=__l&id=__id&date=__date', ['id', 'date'], ['id' => 0, 'date' => date('Y-m-d')]);
+$color = new \Core\Action('main', 'null', 'id=__id&color=__color', 's=__s&l=__l&id=__id&color=__color', ['id', 'color'], ['id' => 0, 'color' => 'white']);
+$user = new \Core\Action('main', 'null', 'id=__id&user=__user', 's=__s&l=__l&id=__id&user=__user', ['id', 'user'], ['id' => 0, 'user' => -1]);
+//$act->link('test', []);
+$date->link('test_1', ['id' => 5, 'date' => '2022-07-5']);
+$color->link('test_2', ['id' => 33, 'color' => 'black']);
+$user->link('test_3', ['id' => 78, 'user' => 13]);
 
